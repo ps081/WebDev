3bc81cdef4e076f72a8c28cceca44c2aee090532
SANDEEP <hitetoshidekesuki@proton.me>
2024-09-22 10:17:06 +0530

    promise

---
+++ Promises/map.js
@@ -0,0 +1,4 @@
+const map = new Map();
+map.set('name', 'Sandeep');
+map.set('age', 21);
+console.log(map.get('name'))
---
+++ Promises/promisified.js
@@ -0,0 +1,12 @@
+function setTimeoutPromisified(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function callback(){
+    console.log("3 seconds have passed")
+}
+
+// setTimeoutPromisified(3000).then(callback)
+
+setTimeoutPromisified(3000).then(()=> console.log("3 seconds are gone"))
+
